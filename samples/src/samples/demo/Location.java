ffb59a0fc2766fa9b2b25d02700f2677c4081d3c
shadman-zoha <shadmanzoha90@gmail.com>
2021-03-18 11:44:40 +0800

    Initial Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,18 @@
+package samples.demo;
+
+public class Location {
+	 private String name; 
+
+	  // Getter
+	  public String getName() {
+	    return name;
+	  }
+
+	  // Setter
+	  public void setName(String newName) {
+	    this.name = newName;
+	  }
+	
+	
+
+}
